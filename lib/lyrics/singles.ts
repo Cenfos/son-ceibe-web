a85e153fa68fc056367d91ec0bf06be523925884
Cenfos <camilo.rguez@gmail.com>
2026-09-09 18:55:20 +0200

    Añadir acordes de Gaivota y letra de A miña terra chama

--- lib/lyrics/singles.ts
+++ lib/lyrics/singles.ts
@@ -167,122 +167,152 @@ latexando o corazón.
   {
     title: "Voa ceibe, ti gaivota",
     album: "Singles",
-    hasChords: false,
-    lyrics: `\\\\[Verso 1]
+    hasChords: true,
+    guitarGuide: {
+      level: "Medio",
+      key: "Sol Mayor (G) / Mi menor (Em)",
+      rhythm: "Rumba gallega / Folk (4/4 animado)",
+      sections: [
+        { title: "Intro e interludio", chords: "| G | D | C | G |" },
+        { title: "Versos 1 y 2", chords: "G · C · G · D / G · C · G · D / Am · Em · C · D" },
+        { title: "Estribillo", chords: "G · D · C · G-D / G · D · C · D-G" },
+        { title: "Solo de acordeón y guitarra", chords: "| G | D | C | G | / | G | D | C-D | G |" },
+        { title: "Verso 3", chords: "G · C · G · D / Am · Em · C · D" },
+        { title: "Cierre", chords: "G · D · G (acorde final rasgueado)" },
+      ],
+    },
+    lyrics: `[Intro]
+| G | D | C | G |
+(Repite según la melodía del acordeón/gaita)
 
+[Verso 1]
+G
 Un vello me dixo un día,
-
+C
 cada quen ten a súa forza,
+G
+a súa maneira de andar, o seu xeito…
+D
+a súa forma de voar!
 
-a súa maneira de andar,
-
-o seu xeito… a súa forma de voar!
-
-
+G
 Pois calquer gaivota sabe,
-
+C
 que ás veces mellor quedar,
-
+G
 se ruxe a treboada,
-
+D
 a choiva te vai levar!
 
-
+Am
 Só tes que agardar un chisco,
-
+Em
 deixas feito o teu camiño,
-
+C
 pois por onde sempre voas…
-
+D
 voltan as vellas historias!
 
-
-\\\\[Estribillo]
-
+[Estribillo]
+G
 Voa ceibe, ti Gaivota,
-
+D
 non te deixes arrastrar,
-
+C
 pois este vento da costa
-
+G  D
 sempre, sempre vai voltar!
 
-
+G
 Pois tes o niño nas rochas
-
+D
 e alí te están a agardar
-
+C
 os teus meniños pequenos,
-
+D  G
 ¡cada quen ten seu fogar!
 
+[Interludio instrumental]
+| G | D | C | G |
 
-\\\\[Verso 2]
-
+[Verso 2]
+G
 Miña gaivota querida,
-
+C
 sempre a terra a sinalar!
-
+G
 Vai ao carón do meu bote,
-
+D
 ¡non me deixes de guiar!
 
-
+G
 Pois sempre que limpo o peixe,
-
+C
 non me esquezo de te ollar,
-
+G
 como vas detrás do barco…
+D
+¡cos golfiños asaltar!
 
-¡cos golfiños a saltar!
-
-
+Am
 E ao chegar por fin ao porto,
-
+Em
 ainda estamos a albiscar,
-
+C
 o arrecendo do teu corpo,
-
+D
 polo ceo a voar!
 
-
-\\\\[Estribillo]
-
+[Estribillo]
+G
 Voa ceibe, ti Gaivota,
-
+D
 non te deixes arrastrar,
-
+C
 pois este vento da costa
-
+G  D
 sempre, sempre vai voltar!
 
-
+G
 Pois tes o niño nas rochas
-
+D
 e alí te están a agardar
-
+C
 os teus meniños pequenos,
-
+D  G
 ¡cada quen ten seu fogar!
 
+[Solo / Solo de Acordeón y Guitarra]
+| G | D | C | G |
+| G | D | C-D | G |
 
-\\\\[Verso 3]
-
+[Verso 3]
+G
 Aprendín da túa vida,
-
+C
 sen palabras ensinar,
-
+G
 que hai ventos que non se vencen...
-
+D
 hai que saber agardar.
 
+Am
 E despois da noite escura,
-
+Em
 sempre volve clarexar,
-
+C
 igual ca branca gaivota
+D
+que non deixa de voar!!!
 
-que non deixa de voar!!!`,
+[Cierre]
+G  D  G
+(Acorde final rasgueado)`,
+  },
+  {
+    title: "A miña terra chama!",
+    album: "Singles",
+    hasChords: false,
+    lyrics: "[Verse 1]\nSaímos catro da casa, unha mochila e ilusión,\nGalicia no retrovisor, Londres no corazón.\nAvión arriba, nubes fóra, mans ben apertadas,\ncomeza a aventura que a familia agardaba...\n\n[Verse 2]\nLondres é un trebón de luces, de linguas e de cor,\ndo Big Ben ao Támesis, todo é un rumor.\nMetro que vai e que vén, nós perdidos e rindo,\nque bonito é perderse se é contigo!\n\n[Pre-Chorus]\nLevo as vosas risas gardadas no peto,\ncada foto, cada paso, cada conto feito.\nPois o mundo é máis fermoso se o vexo convosco,\nsodes o meu mapa, o meu norte e o meu todo!\n\n[Chorus]\nE Londres brilla, si, pero a miña terra chama!\nBrilla Londres, pero o lar é chama!\nMil lembranzas novas que nunca se apagan,\nmais volvemos xuntos, a Nosa Terra chama!\nOh-oh-oh, a Nosa Terra chama!\nOh-oh-oh, onde o mar cantando brama!\n\n[Verse 3 - Rap]\nCamiñamos lado a lado, risas polo camiño,\nfalando da nosa vida, do destino e do cariño.\nBrindamos todos xuntos, sen présa nin apuro,\nporque estes momentos non se mercan con un duro!\n\n[Verse 4 - Rap]\nPaseamos pola historia dun Londres descoñecido,\ncada rúa era un conto, cada paso un sentido,\nChinatown regalounos arrecendos e sabor,\nalí fóisenos a tarde cos farolillos de cor!\n\n[Chorus]\nE Londres brilla, si, pero a miña terra chama!\nBrilla Londres, pero o lar é chama!\nMil lembranzas novas que nunca se apagan,\nmais volvemos xuntos, a Nosa Terra chama!\nOh-oh-oh, a Nosa Terra chama!\nOh-oh-oh, onde o mar cantando brama!\n\n[Verse 5 - Rap]\nSeis pegadas debuxando cada rúa de Londres,\ncada ponte, cada praza, cada un dos seus nomes.\nUnha viaxe en familia non se pode mercar,\nqueda escrita na memoria... pra sempre lembrar.\n\n[Verse 6 - Rap]\nTrouxen o meu mundo enteiro pegadiño ao meu carón,\nlevei pouca equipaxe, pero cheo o corazón.\nNa maleta non collían tantos soños compartidos,\nvosas risas, vosa troula... quedaron sempre conmigo!\n\n[Bridge]\nPodemos cruzar o mundo, cruzar o mar,\npero hai un fío verde que nos vai atar.\nÉ a morriña bonita, que non fai chorar,\né a que che di: xa podes regresar…\nxa podes regresar…\n\n[Final Chorus]\nE Londres brilla, si, pero a miña terra chama!\nBrilla Londres, pero o lar é chama!\nMil lembranzas novas que nunca se apagan,\nmais volvemos xuntos, a Nosa Terra chama!\nOh-oh-oh, a Nosa Terra chama!\nOh-oh-oh, onde o mar cantando brama!\n\n[Outro]\nA miña Terra chama, a miña Terra chama!\nVolver xa á rutina, de feiras, de mariscadas.\nA miña Terra chama, a miña Terra chama!\nFestas son na nosa aldea cando está a tocar a gaita!\nA miña Terra chama, a miña Terra chama!\nTeño sede da lareira, viño en cunca e churrascada!\nA miña Terra chama, a miña Terra chama!\nEsquezamos o asfalto para sentir herba mollada!\n\n[Bridge]\nLevo as vosas risas gardadas no peito,\nsodes o meu mapa, o meu norte e o meu credo!\nHai viaxes que rematan cando chega o avión,\ne outras quedan para sempre gardadas no corazón.\n\n[Chorus]\nE Londres brilla, si, pero a miña terra chama!\nBrilla Londres, pero o lar é chama!\nMil lembranzas novas que nunca se apagan,\nmais volvemos xuntos, a Nosa Terra chama!\nOh-oh-oh, a Nosa Terra chama!\nOh-oh-oh, onde o mar cantando brama!\npero a miña Terra chama!\n(Aii-aii-aii- aiii – aiiiiii...! )\nA miña terra chama!\nA miña terra chama!\nA miña terra chama!",
   },
 ]
-
