dd59141e9bfbefe588495bef67a6dfabdbda76c1
Cenfos <103105939+Cenfos@users.noreply.github.com>
2026-09-15 19:09:34 +0200

    feat: add As dúas Marías lyrics

---
+++ lib/lyrics/as-duas-marias.ts
@@ -0,0 +1,99 @@
+import type { SongLyric } from "./types"
+
+export const asDuasMariasLyrics: SongLyric[] = [
+  {
+    title: "As dúas Marías",
+    album: "Singles",
+    collections: ["Singles"],
+    hasChords: false,
+    lyrics: `Dúas sombras pola rúa,
+dous silencios ao pasar.
+Compostela bate a néboa,
+e dous ollos para mirar.
+
+Foron Maruxa e Corália,
+camiñando man a man.
+Levan fame, levan berros,
+mais xuntas elas están.
+
+Naceron nuns tempos malos,
+décadas escuras son.
+Casa de xente rebelde,
+moito iso non axudou.
+
+E chegou o mes de xullo,
+e ese medo alí quedou.
+Moitos lles berraban “roxas”,
+mais o seu sangue non dobrou.
+
+E Compostela calaba,
+as pedras sabían falar.
+Había portas ben pechadas
+e ollos que non queren mirar.
+
+E alí van as dúas Marías
+por Compostela andar.
+As flores levan no pelo,
+mais o mundo queda atrás.
+
+Que ninguén borra os camiños,
+nin as pegadas ao andar,
+pois quen loita por ser Ceibe,
+non se pode silenciar!
+
+As Marías, dúas mulleres,
+dous corazóns contra o chan.
+Medo e morte percorrían,
+foi loita por respirar.
+
+Pero o tempo non esquece,
+as pedras saben gardar
+as historias dos que loitan,
+as dúas xuntas, man a man.
+
+E un día cambiou o xeito
+no que a cidade as mirou.
+Xa non eran inimigas,
+pois a razón afincou.
+
+Dúas figuras na Alameda
+ben se vían ao pasar,
+vendo a xente que camiña,
+Compostela está a espertar.
+
+E alí van as dúas Marías
+por Compostela andar.
+As flores levan no pelo,
+mais o mundo queda atrás.
+
+Que ninguén borra os camiños,
+nin as pegadas ao andar,
+pois quen loita por ser Ceibe,
+non se pode silenciar!
+
+As Marías, dúas mulleres,
+dous corazóns contra o chan.
+Medo e morte percorrían,
+foi loita por respirar.
+
+Agora son elas dúas,
+que nos ven ao seu pasar.
+Non lles preguntes que foron,
+pregunta por que calar.
+
+Maruxa e Corália foron,
+dúas sombras que hoxe están,
+mirando pra Compostela,
+na Alameda, man a man.
+
+E alí van as dúas Marías
+por Compostela andar.
+As flores levan no pelo,
+mais o mundo queda atrás.
+
+Que ninguén borra os camiños,
+nin as pegadas ao andar,
+pois quen loita por ser Ceibe,
+non se pode silenciar!`,
+  },
+]
